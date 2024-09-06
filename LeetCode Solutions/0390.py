516fea584cf69b480fd7cda1b49be29afb7fbf03
Raza <141234596+KMORaza@users.noreply.github.com>
2024-09-06 19:20:09 +0530

    Add files via upload

--- LeetCode Solutions/0390.py
+++ LeetCode Solutions/0390.py
@@ -1,9 +1,13 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        def largest_power_of_2(x):
-            power = 1
-            while power <= x:
-                power <<= 1
-            return power >> 1
-        largest_power = largest_power_of_2(n)
-        return 2 * (n - largest_power) + 1
+        start = 1
+        step = 1
+        left_to_right = True
+        while n > 1:
+            if left_to_right or n % 2 == 1:
+                start += step
+            n //= 2
+            step *= 2
+            left_to_right = not left_to_right
+        return start
+
